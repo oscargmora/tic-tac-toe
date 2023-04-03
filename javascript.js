954542e444a4461589105a263ee55854ecf3e0bf
Oscar Mora <oscgmora@gmail.com>
2023-04-03 00:33:56 -0400

    Add Win Conditions
    This commit adds win conditions to the game. With this commit, the game is able to be played in the console with the winner displayed. This also uses ESLint and Prettier to format the code.

--- javascript.js
+++ javascript.js
@@ -1,53 +1,143 @@
-const gameBoard = (() => {
-    const row = 3;
-    const column = row;
-    const _board = [];
-  
-    for (let i = 0; i < row; i++) {
-        _board[i] = [];
-        for (let j = 0; j < column; j++) {
-            _board[i].push(0);
-        };
-    };
-  
-    const dropToken = (row, column, playerToken) => {
-        _board[row][column] = playerToken;
-        console.log(_board);
-    };
-  
-    const displayBoard = () => _board;
-    
-    console.log(_board);
-    return {dropToken};
-})();
-
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-use-before-define */
+/* eslint-disable consistent-return */
+/* eslint-disable no-shadow */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-underscore-dangle */
+let round = 0;
 
 const Player = (token) => {
     this.token = token;
-    const getToken = () => {return token};
-    return {getToken};
+    const getToken = () => token;
+    return { getToken };
 };
 
-
 const gameController = (() => {
-    const playerOne = Player('X');
-    const playerTwo = Player('O');
-    let round = 0;
-    
-    const getActivePlayerToken = () => {
-        return round % 2 === 0 ? playerOne.getToken() : playerTwo.getToken();
-    };
-    
+    const _playerOne = Player('X');
+    const _playerTwo = Player('O');
+
+    // prettier-ignore
+    const getActivePlayerToken = () => round % 2 === 0 ? _playerOne.getToken() : _playerTwo.getToken();
+
     const playRound = (row, column) => {
         if (row > 2 || column > 2) return console.log('Invalid Entry');
         if (round > 9) return;
         gameBoard.dropToken(row, column, getActivePlayerToken());
-        if (round === 8) return console.log("Tie Game");
-        round++;
-        console.log(`${getActivePlayerToken()}'s Turn`);
     };
 
-    console.log(`${getActivePlayerToken()}'s Turn`);
-    
-    return {playRound};
+    return { playRound, getActivePlayerToken };
+})();
+
+const gameBoard = (() => {
+    const row = 3;
+    const column = row;
+    const _board = [];
+
+    let _win = false;
+
+    function _playerTurnDisplay() {
+        if (_win) return;
+
+        if (round === 9) {
+            console.log("It's a Tie!");
+            return round++;
+        }
+
+        console.log(`${gameController.getActivePlayerToken()}'s Turn`);
+    }
+
+    for (let i = 0; i < row; i++) {
+        _board[i] = [];
+
+        for (let j = 0; j < column; j++) {
+            _board[i].push(0);
+        }
+    }
+
+    const _checkRows = (board) => {
+        for (const row of board) {
+            if (
+                row.every((val) => val === 'X') ||
+                row.every((val) => val === 'O')
+            ) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    const _checkColumns = (board) => {
+        const numRows = board.length;
+        const numColumns = board[0].length;
+
+        for (let i = 0; i < numColumns; i++) {
+            const column = [];
+            for (let j = 0; j < numRows; j++) {
+                column.push(board[j][i]);
+            }
+            if (
+                column.every((val) => val === 'X') ||
+                column.every((val) => val === 'O')
+            ) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    const _checkDiagonals = (board) => {
+        const diagonal1 = [board[0][0], board[1][1], board[2][2]];
+        const diagonal2 = [board[0][2], board[1][1], board[2][0]];
+
+        if (
+            diagonal1.every((val) => val === 'X') ||
+            diagonal1.every((val) => val === 'O')
+        ) {
+            return true;
+        }
+        if (
+            diagonal2.every((val) => val === 'X') ||
+            diagonal2.every((val) => val === 'O')
+        ) {
+            return true;
+        }
+        return false;
+    };
+
+    const _checkForWin = (board) => {
+        if (
+            _checkRows(board) ||
+            _checkColumns(board) ||
+            _checkDiagonals(board)
+        ) {
+            _win = true;
+            console.log(`${gameController.getActivePlayerToken()} Wins!`);
+            return true;
+        }
+        return false;
+    };
+
+    const dropToken = (row, column, playerToken) => {
+        if (_win) return;
+
+        if (_board[row][column] !== 0) {
+            console.log('Spot Already Taken');
+            return console.log(_board);
+        }
+
+        _board[row][column] = playerToken;
+
+        _checkForWin(_board);
+
+        console.log(_board);
+
+        round++;
+
+        _playerTurnDisplay();
+    };
+
+    console.log(_board);
+    _playerTurnDisplay();
+
+    return { dropToken };
 })();
